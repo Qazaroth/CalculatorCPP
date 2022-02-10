150fbd9f6196277b9c21924ea9d0946f570630c9
Qazaroth <realthedarksid3r@gmail.com>
2022-02-10 19:27:48 +0800

    Basic addition

--- Calculator.cpp
+++ Calculator.cpp
@@ -12,11 +12,19 @@ void displayTitle()
 
 int main()
 {
-	int firstNo, lastNo;
+	int firstNo, secondNo;
 
 	displayTitle();
 
+	print("What's the first number?");
 	std::cin >> firstNo;
+
+	print("What's the second number?");
+	std::cin >> secondNo;
+
+	int output = firstNo + secondNo;
+
+	print("The sum of (" + std::to_string(firstNo) + " and " + std::to_string(secondNo) + ") is " + std::to_string(output));
 
 	return 0;
 }
